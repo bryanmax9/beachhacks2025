69f3b890f87b759bcdabb6231abc1c19a5f435c2
xiuv <333harvest.moon333@gmail.com>
2024-12-31 11:35:41 -0800

    prizes

---
+++ src/app/(landing)/_components/prizes.tsx
@@ -0,0 +1,10 @@
+export default function Prizes() {
+    return (
+        <div>
+            <h1 className="font-bold text-5xl text-center">$100000</h1>
+            <div className="flex">
+
+            </div>
+        </div>
+    )
+}
