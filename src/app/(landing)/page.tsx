ac8614cfd2912ca181cb766b1499cce5a2b98d53
xiuv <333harvest.moon333@gmail.com>
2024-12-31 11:44:43 -0800

    prize widget

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,9 +1,7 @@
 import Hero from "@/app/(landing)/_components/hero";
-import Prizes from "./_components/prizes";
 export default function Home() {
   return (
     <div>
-      <Prizes/>
       <Hero />
       <div className="h-[200vh] flex items-center justify-center text-lg">
         Scroll down to see the navbar change!
