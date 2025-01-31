dab3fb23ca8167913d2675cbfb3d1393b0c7e3fd
xiuv <333harvest.moon333@gmail.com>
2025-01-31 13:28:55 -0800

    merged landing and prizes

--- src/app/(landing)/_components/teams.tsx
+++ src/app/(landing)/_components/teams.tsx
@@ -1,9 +1,47 @@
 import TeamWidget from "./team_widget"
 import pfp from "../_images/profilepic.png"
+import green_fish from "../../../../public/green_fish.png"
+import Image from "next/image"
+import star_fish from "../../../../public/star_fish.png"
+import jelly_fish from "../../../../public/jellyfish.png"
 export default function TeamsComponent() {
     return(
-        <div className="flex flex-wrap gap-x-16 gap-y-10 justify-center align-center items-center mt-1">
-            <TeamWidget name="first last" role="dev" image={pfp}/>
+        <div className="relative">
+            <h1 className="font-bold text-5xl text-center">Teams</h1>
+            <div className="md:w-32 w-24 md:h-32 h-24 absolute z-10 bottom-36 md:right-10 right-5">
+            <Image 
+            src={star_fish}
+            alt="star_fish"
+            layout="fill"
+            objectFit="fill"
+            />
+            </div>
+            <div className="md:w-32 w-24 md:h-32 h-24 absolute z-10 md:bottom-36 bottom-[-150px] md:left-10 left-5">
+            <Image 
+            src={jelly_fish}
+            alt="jelly_fish"
+            layout="fill"
+            objectFit="fill"
+            />
+            </div>
+
+            <div className="md:w-32 w-24 md:h-32 h-24 absolute z-10 md:top-20 top-80 md:left-5 left-2">
+            <Image 
+            src={green_fish}
+            alt="green_fish"
+            layout="fill"
+            objectFit="fill"
+            />
+            </div>
+
+            <div className="flex flex-wrap md:px-28  align-middle items-center justify-center md:flex-row flex-col md:gap-x-14 gap-y-5 mt-5">
+                <TeamWidget name="first last" role="dev" image={pfp}/>
+                <TeamWidget name="first last" role="dev" image={pfp}/>
+                <TeamWidget name="first last" role="dev" image={pfp}/>
+                <TeamWidget name="first last" role="dev" image={pfp}/>
+                <TeamWidget name="first last" role="dev" image={pfp}/>
+                <TeamWidget name="first last" role="dev" image={pfp}/>
+            </div>
         </div>
     )
 }
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,11 +1,14 @@
 import Hero from "@/app/(landing)/_components/hero";
+import Prizes from "./_components/prizes";
+import Teams from "./_components/teams";
 export default function Home() {
   return (
     <div>
       <Hero />
       <div className="h-[200vh] flex items-center justify-center text-lg">
-        Scroll down to see the navbar change!
       </div>
+      <Prizes/>
+      <Teams/>
     </div>
   );
 }
