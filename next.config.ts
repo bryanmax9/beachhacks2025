f2c974e100f34927541f56d25c92a63b15621d7a
bazalu <bazaluu@gmail.com>
2025-02-02 18:20:18 -0800

    update: eslint

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-    ignoreDuringBuilds: true,
-};
+module.exports = nextConfig
 
-export default nextConfig;
