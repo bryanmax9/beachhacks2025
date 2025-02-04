d299c88a900c1bd96a6ea417fad63675c0a51249
xiuv <333harvest.moon333@gmail.com>
2025-02-03 22:07:58 -0800

    prize fix

--- src/app/(landing)/_components/prizes.tsx
+++ src/app/(landing)/_components/prizes.tsx
@@ -1,14 +1,14 @@
 
-import PrizeWidget from "./prize_widget"
 import red_fish from "../../../../public/red_fish.png"
 import star_fish from "../../../../public/star_fish.png"
+import "./prizes.css"
 import Image from "next/image"
 export default function Prizes() {
     return (
         <div className="relative flex items-center flex-col justify-center align-middle w-full">
             <h2 className="text-3xl font-bold mb-4 text-blue-900 bg-white bg-opacity-90 p-2 shadow-xl rounded-xl w-fit">Prizes (Coming soon!)</h2>
             {/* star fish container to scale on mobile */}
-            <div className="md:w-32 w-24 md:h-32 h-24 absolute z-10 bottom-20 md:left-10 left-5 float-animation">
+            <div className="floating md:w-32 w-24 md:h-32 h-24 absolute z-10 bottom-4 md:left-10 left-5 ">
                 <Image
                     src={star_fish}
                     alt="star_fish"
@@ -16,7 +16,7 @@ export default function Prizes() {
                     objectFit="fill"
                 />
             </div>
-            <div className="md:w-32 w-24 md:h-32 h-24 absolute z-10 md:top-10 top-80 md:right-10 right-4 float-animation">
+            <div className="floating md:w-32 w-24 md:h-32 h-24 absolute z-10 md:top-10 top-80 md:right-10 right-4 ">
                 <Image
                     src={red_fish}
                     alt="red_fish"
