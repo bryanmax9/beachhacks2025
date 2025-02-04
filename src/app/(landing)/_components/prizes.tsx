8d9914d21554847de8037ce0454cdd98009c7ec3
xiuv <333harvest.moon333@gmail.com>
2025-02-03 22:01:17 -0800

    test

--- src/app/(landing)/_components/prizes.tsx
+++ src/app/(landing)/_components/prizes.tsx
@@ -6,7 +6,7 @@ import Image from "next/image"
 export default function Prizes() {
     return (
         <div className="relative flex items-center flex-col justify-center align-middle w-full">
-            <h2 className="text-3xl font-bold mb-4 text-blue-900 bg-white bg-opacity-90 p-2 shadow-xl rounded-xl w-fit">Prizes</h2>
+            <h2 className="text-3xl font-bold mb-4 text-blue-900 bg-white bg-opacity-90 p-2 shadow-xl rounded-xl w-fit">Prizes (Coming soon!)</h2>
             {/* star fish container to scale on mobile */}
             <div className="md:w-32 w-24 md:h-32 h-24 absolute z-10 bottom-20 md:left-10 left-5 float-animation">
                 <Image
@@ -25,11 +25,6 @@ export default function Prizes() {
                 />
             </div>
             <div className="flex flex-wrap md:px-28  align-middle items-center justify-center md:flex-row flex-col md:gap-x-14 gap-y-5 mt-5">
-                <PrizeWidget name="iphone" image={star_fish}/>
-                <PrizeWidget name="$100000" image={star_fish}/>
-                <PrizeWidget name="new car" image={star_fish}/>
-                <PrizeWidget name="a house" image={star_fish}/>
-                <PrizeWidget name="tuition paid" image={star_fish}/>
             </div>
         </div>
     )
