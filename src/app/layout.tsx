e0b3b02e19783019cd5da125d07092bb6b29fa19
Charles Milton <139507918+chuckmilton@users.noreply.github.com>
2025-02-23 15:00:53 -0800

    Add more favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,10 @@ export default function RootLayout({ children }: RootLayoutProps) {
           href="https://fonts.googleapis.com/css2?family=Dynapuff&display=swap"
           rel="stylesheet"
         />
-        <link rel="icon" href="/logo.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <title>BeachHacks 2025 - CSULB</title>
       </head>
 
