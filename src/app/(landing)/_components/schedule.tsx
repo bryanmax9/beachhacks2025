39636d315551ff267b902a345722a2175bac2f51
harvset7777 <333harvest.moon333@gmail.com>
2025-03-17 08:56:58 -0700

    schedule

--- src/app/(landing)/_components/schedule.tsx
+++ src/app/(landing)/_components/schedule.tsx
@@ -5,9 +5,11 @@ export default function Schedule() {
   const schedule = [
     {
       day: "Saturday 3/22", events: [
-        { time: "9:00 AM - 10:00 AM", event: "Check in" },
+        { time: "*9:00 AM - 10:00 AM", event: "Check in" },
         { time: "10:00 AM - 10:30 AM", event: "Opening Ceremony" },
-        { time: "10:30 AM", event: "Hacking Begins! Good luck and have fun :)" },
+        { time: "*10:30 AM", event: "Hacking Begins! Good luck and have fun :)" },
+        { time: "10:45 AM	- 11:45 AM", event: "Patient Safety Workshop" },
+        { time: "12:00 PM -	1:00 PM", event: "Dain AI Workshop" },
         { time: "1:00 PM - 1:30 PM", event: "Lunch" },
         { time: "1:30 PM - 2:30 PM", event: "GitHub Workshop" },
         { time: "2:45 PM - 3:45 PM", event: "API Workshop" },
@@ -22,7 +24,7 @@ export default function Schedule() {
     {
       day: "Sunday 3/23", events: [
         { time: "7:00 AM - 8:00 AM", event: "Breakfast" },
-        { time: "10:00 AM - 11:00 AM", event: "Project Submissions" },
+        { time: "*10:00 AM - 11:00 AM", event: "Project Submissions" },
         { time: "11:00 AM - 12:30 PM", event: "Judging" },
         { time: "12:30 PM - 1:00 PM", event: "Closing Ceremony" }
       ]
